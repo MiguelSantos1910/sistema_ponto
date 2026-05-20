6fda4e517634fc14cce16c406321ab75022ceba7
MiguelSantos1910 <miguelsantos51346@gmail.com>
2026-05-20 15:13:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
+    const win = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        minWidth: 1000,
+        minHeight: 700,
+        icon: path.join(__dirname, "assets", "logo.avif"),
+        webPreferences: {
+            contextIsolation: true
+        }
+    });
 
-  win.loadFile('./frontend/menu.html');
+    win.loadFile("frontend/login.html");
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
